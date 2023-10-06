5cb0990edb9c8f8a54b1befd2bef83c9578688b4
fraulueneburg <github@fraulueneburg.de>
2023-10-06 23:07:29 +0200

    added IPA form

--- src/components/FormAddIPA.jsx
+++ src/components/FormAddIPA.jsx
@@ -33,21 +33,78 @@ function FormAddIPA({ onSubmit }) {
 
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="letter">letter</label>
-				<input id="letter" type="text" name="letter" placeholder="Letter" value={newIPAobj.letter} onChange={handleChange} />
-				<textarea
-					name="pronounciation"
-					value={newIPAobj.pronounciation}
-					onChange={handleChange}
-					placeholder="pronounciation"
-				/>
-				<textarea name="approximation" value={newIPAobj.approximation} onChange={handleChange} placeholder="approximation" />
-				<input type="text" name="IPAname" value={newIPAobj.IPAname} onChange={handleChange} placeholder="IPA Name" />
-				<input type="number" name="sound" value={newIPAobj.sound} onChange={handleChange} placeholder="IPA Number" />
-				<input type="file" name="file" value={newIPAobj.file} onChange={handleChange} placeholder="Sound" />
+			<form onSubmit={handleSubmit} className="add-ipa">
+				<div className="columns">
+					<div className="column is-2">
+						<label htmlFor="ipa_letter">Letter</label>
+						<input
+							id="ipa_letter"
+							type="text"
+							name="letter"
+							placeholder="Letter"
+							value={newIPAobj.letter}
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="column is-2">
+						<label htmlFor="ipa_ipaNumber">IPA Number</label>
+						<input
+							id="ipa_ipaNumber"
+							type="number"
+							name="sound"
+							value={newIPAobj.sound}
+							onChange={handleChange}
+							placeholder="IPA Num"
+						/>
+					</div>
+					<div className="column is-8">
+						<label htmlFor="ipa_ipaName">IPA Name</label>
+						<input
+							id="ipa_ipaName"
+							type="text"
+							name="IPAname"
+							value={newIPAobj.IPAname}
+							onChange={handleChange}
+							placeholder="IPA Name"
+						/>
+					</div>
+				</div>
+				<div className="columns">
+					<div>
+						<label htmlFor="ipa_pronounciation">Pronounciation</label>
+						<textarea
+							id="ipa_pronounciation"
+							name="pronounciation"
+							value={newIPAobj.pronounciation}
+							onChange={handleChange}
+							placeholder="pronounciation"
+						/>
+					</div>
+					<div>
+						<label htmlFor="ipa_approximation">Approximation</label>
+						<textarea
+							id="ipa_approximation"
+							name="approximation"
+							value={newIPAobj.approximation}
+							onChange={handleChange}
+							placeholder="approximation"
+						/>
+					</div>
+					<div>
+						<label htmlFor="ipa_sound">Sound</label>
+						<input
+							id="ipa_sound"
+							type="file"
+							name="file"
+							value={newIPAobj.file}
+							onChange={handleChange}
+							placeholder="Sound"
+							accept="audio/*"
+						/>
+					</div>
+				</div>
 				<button type="submit" value="create new IPA">
-					create new IPA
+					add new IPA
 				</button>
 			</form>
 			<br />
