bcf0a7898489d269f204cf145c103bc5457c59f0
fraulueneburg <github@fraulueneburg.de>
2023-10-06 23:07:10 +0200

    moved filter input field

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -9,46 +9,49 @@ function Table(props) {
 	return (
 		<>
 			{isDataValid ? (
-				<figure className="table">
-					<table>
-						<thead>
-							<tr>
-								{dataKeys.map((singleKey) => {
-									return <th key={nanoid()}>{singleKey}</th>
+				<>
+					<input type="search" placeholder="filter" />
+					<figure className="table">
+						<table>
+							<thead>
+								<tr>
+									{dataKeys.map((singleKey) => {
+										return <th key={nanoid()}>{singleKey}</th>
+									})}
+								</tr>
+							</thead>
+							<tbody>
+								{data.map((singleObj, index) => {
+									return (
+										<tr key={nanoid()}>
+											{Object.values(data[index]).map((objectValue, i) => {
+												let cellContent = ''
+
+												if (dataKeys[i] === 'Sound') {
+													cellContent = <Audio data={objectValue} />
+												} else if (Array.isArray(objectValue)) {
+													cellContent = objectValue.join(', ')
+												} else {
+													cellContent = objectValue
+												}
+
+												return i === 0 ? (
+													<th className={dataKeys[i]} key={nanoid()}>
+														{cellContent}
+													</th>
+												) : (
+													<td className={dataKeys[i]} key={nanoid()}>
+														{cellContent}
+													</td>
+												)
+											})}
+										</tr>
+									)
 								})}
-							</tr>
-						</thead>
-						<tbody>
-							{data.map((singleObj, index) => {
-								return (
-									<tr key={nanoid()}>
-										{Object.values(data[index]).map((objectValue, i) => {
-											let cellContent = ''
-
-											if (dataKeys[i] === 'Sound') {
-												cellContent = <Audio data={objectValue} />
-											} else if (Array.isArray(objectValue)) {
-												cellContent = objectValue.join(', ')
-											} else {
-												cellContent = objectValue
-											}
-
-											return i === 0 ? (
-												<th className={dataKeys[i]} key={nanoid()}>
-													{cellContent}
-												</th>
-											) : (
-												<td className={dataKeys[i]} key={nanoid()}>
-													{cellContent}
-												</td>
-											)
-										})}
-									</tr>
-								)
-							})}
-						</tbody>
-					</table>
-				</figure>
+							</tbody>
+						</table>
+					</figure>
+				</>
 			) : (
 				<p>Invalid table data</p>
 			)}
